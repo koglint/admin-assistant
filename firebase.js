e56d7d3f214d5c8ca923c1cb3159305a5faa52de
koglint <troy.koglin1@det.nsw.edu.au>
2025-06-21 22:06:18 +1000

    update

--- firebase.js
+++ firebase.js
@@ -1,21 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyATW9PpkwE7r7XTrlq1oKWMhHAEe-JNNec",
   authDomain: "admin-assistant-6dd20.firebaseapp.com",
   projectId: "admin-assistant-6dd20",
-  storageBucket: "admin-assistant-6dd20.firebasestorage.app",
+  storageBucket: "admin-assistant-6dd20.appspot.com",
   messagingSenderId: "1058950484184",
   appId: "1:1058950484184:web:c6f16e085cdb433c2c07ad",
   measurementId: "G-9T8CE5Y6PP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  auth,
+  db,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+};
